17a30215820108fced4ba2ddf22fd9be4abef96a
Keith Walker <walkerk1980@users.noreply.github.com>
2019-12-13 15:17:38 -0800

    fixed errors with regions, excluded accounts, disabled regions

--- run_in_org.py
+++ run_in_org.py
@@ -16,15 +16,21 @@ ORGS_ACCESS_ROLE_NAME = 'OrganizationAccountAccessRole'
 
 SERVICE_NAME = 'ec2'
 ACTION_NAME = 'describe_subnets'
-EXCLUDED_ACCOUNT_IDS = []
+
+# list of accounts that you want to exclude
+EXCLUDED_ACCOUNT_IDS = ['720840927657', '414899279353', '946508719051']
 
 # list of regions to run script in
-service_regions=boto3.session.Session().get_available_regions('ec2')
+service_regions=boto3.session.Session().get_available_regions(SERVICE_NAME)
 # disable above and set manually if service doesn't support all the regions that EC2 does
 # uncomment the next 3 lines to set regions manually
 # regions = 'ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 \
 # eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2'
 # service_regions = regions.split()
+
+# remove regions that are not enabled
+service_regions.remove('ap-east-1')
+service_regions.remove('me-south-1')
 
 continue_on_error = None
 
@@ -58,32 +64,32 @@ for account in account_ids:
     account_orgs_role_arn = 'arn:aws:iam::' + \
         account + ':role/' + ORGS_ACCESS_ROLE_NAME
     try:
-        if account not in [EXCLUDED_ACCOUNT_IDS, master_account_id]:
+        if account not in [master_account_id] and account not in EXCLUDED_ACCOUNT_IDS:
             credentials = sts.assume_role(
                 RoleArn=account_orgs_role_arn,
                 RoleSessionName='RunInOrgScript',
-            )['Credentials']
-            member_session = boto3.Session(
-                                         aws_access_key_id=credentials['AccessKeyId'],
-                                         aws_secret_access_key=credentials['SecretAccessKey'],
-                                         aws_session_token=credentials['SessionToken'],
-                                         )
+            ).get('Credentials')
             for region in service_regions:
+                member_session = boto3.Session(region_name=region,
+                                         aws_access_key_id=credentials.get('AccessKeyId'),
+                                         aws_secret_access_key=credentials.get('SecretAccessKey'),
+                                         aws_session_token=credentials.get('SessionToken'),
+                                         )
                 member_client = member_session.client(SERVICE_NAME, region_name=region)
-                print('Running : ' + region)
+                print('Running in : ' + region)
                 # TODO: Action is not paginated, add pagination logic
                 # if action does not support pagination continue as normal
                 action = getattr(member_client, ACTION_NAME)
-            print(action())
+                print(action())
         if account == master_account_id:
-            master_session = boto3.Session()
             # TODO: Action is not paginated, add pagination logic
             # if action does not support pagination continue as normal
             for region in service_regions:
-                print('Running : ' + region)
+                master_session = boto3.Session(region_name=region)
+                print('Running in : ' + region)
                 master_client = master_session.client(SERVICE_NAME, region_name=region)
                 action = getattr(master_client, ACTION_NAME)
-            print(action())
+                print(action())
     except Exception as e:
         print(e)
         print('An error occoured in ' + account)
